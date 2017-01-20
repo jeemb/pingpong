3a651371eac385f8053235983009e123c3b86be5
jb <jenniferbeem@gmail.com>
2017-01-20 09:43:17 -0800

    add createArray function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// business logic
+var createArray = function(number) {
+  var result = '';
+  var high = number;
+  var array = [];
+  for (var i=0; i <= high; i++) {
+    array.push(i);
+  }
+
+  
