7fbebecc6e7bc4171afac921bf7f23239ea431b6
jb <jenniferbeem@gmail.com>
2017-01-20 09:44:10 -0800

    create initial front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,32 @@ var createArray = function(number) {
     array.push(i);
   }
 
-  
+  // for (var i=0; i <= array.length; i++) {
+  //   if (number[i] % 3 === 0) {
+  //     return "ping";
+  //   } else if (number[i] % 5 === 0) {
+  //     return "pong";
+  //   } else if (number[i] % 3 === 0 && number[i] % 5 === 0) {
+  //     return "pingpong";
+  //   }
+  // }
+  return result;
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var inputNumber = parseInt($("input#number").val());
+    var result = createArray(inputNumber);
+    // if (result === "ping") {
+    //   $(".result").text("ping")
+    // } else if (result === "pong") {
+    //   $(".result").text("pong")
+    // } else if (result === "pingpong") {
+    //   $(".result").text("pingpong")
+    // }
+
+    $("#result").show();
+});
+});
